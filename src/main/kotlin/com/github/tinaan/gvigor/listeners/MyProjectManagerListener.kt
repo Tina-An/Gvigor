52214191f57edd9c4af96f17c45f295c28b20cbf
GitHub Action <action@github.com>
2021-12-13 03:35:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/tinaan/gvigor/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/tinaan/gvigor/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tinaan.gvigor.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tinaan.gvigor.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/tinaan/gvigor/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tinaan/gvigor/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tinaan.gvigor.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tinaan.gvigor.MyBundle
 
 class MyProjectService(project: Project) {
 

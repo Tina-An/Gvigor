6a5e9d498f8afc5ae6d2486d6fb43101bc3ad954
dongmengyuan <dongmengyuan@huawei.com>
2021-12-17 15:11:42 +0800

    修改gradle.kts文件，配置仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("http://artifactory.cde.huawei.com/artifactory/maven-public/")
+            isAllowInsecureProtocol = true
+        }
+        maven {
+            url = uri("http://artifactory.cde.huawei.com/artifactory/jetbrains-public/")
+            isAllowInsecureProtocol = true
+        }
+        maven {
+            url = uri("http://artifactory.cde.huawei.com/artifactory/intellij-dependencies/")
+            isAllowInsecureProtocol = true
+        }
+    }
+}
+
 rootProject.name = "Gvigor"
